e19afeb913c3b4603f4fdb2c4f9128757b9424cc
Dmitry <takesxq77@gmail.com>
2025-01-31 16:08:52 +0300

    before new_start

---
+++ database/keup.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 31 2025 г., 14:22
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `keup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students`
+--
+
+CREATE TABLE `students` (
+  `chat_id` bigint(20) NOT NULL,
+  `student_number` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `group` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_headman` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `chat_id` bigint(20) NOT NULL,
+  `surname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`chat_id`),
+  ADD UNIQUE KEY `student_number` (`student_number`);
+
+--
+-- Индексы таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`chat_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
